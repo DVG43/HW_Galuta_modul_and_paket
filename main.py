7c3c1dfbf04440d82644a0aec029b028786d727a
Galuta Dmitrii <dvgaluta@gmail.com>
2022-03-04 23:18:05 +0300

    commit_3

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 from application.db.people import printing_people
 from application.salary import getin_many
-
+from datetime import datetime, date, time
 
 if __name__ == '__main__':
     name = input('введите имя')
     many = int(input('введите деньги в час'))
     printing_people(name)
     getin_many(name, many)
+    print(datetime.today())
 
 
 
